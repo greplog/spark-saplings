0c7d78f576c20afc94023fb2acc1efc69b8fdef0
sachin goyal <sachingoyal.422@gmail.com>
2021-07-12 10:52:02 +0530

    Added MnMCount Application code

--- build.sbt
+++ build.sbt
@@ -2,4 +2,10 @@ name := "spark-saplings"
 
 version := "0.1"
 
-scalaVersion := "2.13.6"
+scalaVersion := "2.12.10"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "3.0.0",
+  "org.apache.spark" %% "spark-sql" % "3.0.0",
+  "com.datastax.spark" %% "spark-cassandra-connector" % "3.0.1"
+)
--- src/main/scala/DateOperations.scala
+++ src/main/scala/DateOperations.scala
@@ -1,24 +1,32 @@
 import java.text.SimpleDateFormat
 import java.util.Date
+import org.apache.spark.sql.SparkSession
+import com.datastax.spark.connector._
+
 
 //this code is a rough example of how to create an RDD from underlying database
 // and then apply filter, map, then collect collect count of each value
 
 //TODO : this code has compile errors which need to be fixed later on
 
-class DateOperations {
+object DateOperations {
 
-  val sampleTable = sc.cassandraTable("my_db", "sample_table");
+  def main(args: Array[String]) {
 
-  val format = "yyyy-MM-dd"
-  val dateFormat = new SimpleDateFormat(format)
+    val spark = SparkSession.builder.appName("DateOperations").getOrCreate()
+    val sc = spark.sparkContext
+    val sampleTable = sc.cassandraTable("my_db", "sample_table")
 
-  sampleTable.filter(row => {
-    val booleanColumn = row.get[Option[Boolean]]("boolean_column").getOrElse(false)
-    !booleanColumn
-  }).map( row => {
-    val timestampColumn = row.get[Option[java.util.Date]]("timestamp_column").getOrElse(new Date())
-    val dateString = dateFormat.format(timestampColumn)
-    dateString
-  }).filter(x => x.compareTo("2021-06-03") > 0).countByValue()
+    val format = "yyyy-MM-dd"
+    val dateFormat = new SimpleDateFormat(format)
+
+    sampleTable.filter(row => {
+      val booleanColumn = row.get[Option[Boolean]]("boolean_column").getOrElse(false)
+      !booleanColumn
+    }).map(row => {
+      val timestampColumn = row.get[Option[java.util.Date]]("timestamp_column").getOrElse(new Date())
+      val dateString = dateFormat.format(timestampColumn)
+      dateString
+    }).filter(x => x.compareTo("2021-06-03") > 0).countByValue()
+  }
 }
